4b0e5d3c057531a90ee03e49edfa076939835a0d
daniel <fguirol@gmail.com>
2023-06-13 22:07:35 +0200

    Pipe & Promise to display values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   isAuth = false;
 
+  //Pipe Date
+  //lastUpdate = new Date();
+
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+      const date = new Date();
+      setTimeout(
+        () => {
+          resolve(date);
+        }, 2000
+      );
+    }
+  )
   appareils = [
     {
       name: 'Television',
