60f57df2cf58d23091adf4561ee6c247cebc4a3e
daniel <fguirol@gmail.com>
2023-06-14 17:18:06 +0200

    adding auth and view app component to handle routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,40 +7,7 @@ import {AppareilService} from "./service/appareil.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  isAuth = false;
-
-  //Pipe Date
-  //lastUpdate = new Date();
-
-  lastUpdate = new Promise(
-    (resolve, reject) => {
-      const date = new Date();
-      setTimeout(
-        () => {
-          resolve(date);
-        }, 2000
-      );
-    }
-  )
-  appareils: any[];
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 9000
-    );
-  }
-
   ngOnInit(): void {
-    this.appareils = this.appareilService.appareils;
   }
 
-
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre(){
-    this.appareilService.switchOffAll();
-  }
 }
