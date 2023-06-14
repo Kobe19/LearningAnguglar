d20662be74b4a23dc5b5f610317f97b9d4d6c6b9
daniel <fguirol@gmail.com>
2023-06-14 17:10:14 +0200

    switch each appareil status

--- src/app/service/appareil.service.ts
+++ src/app/service/appareil.service.ts
@@ -1,3 +1,37 @@
 export class AppareilService{
 
+  appareils = [
+    {
+      name: 'Television',
+      status: 'éteint'
+    },
+    {
+      name: 'PS5',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  switchOnAll(){
+    for(let app of this.appareils){
+      app.status = 'allumé'
+    }
+  }
+
+  switchOffAll(){
+    for(let app of this.appareils){
+      app.status = 'éteint'
+    }
+  }
+
+  SwitchOnOne(index: number){
+    this.appareils[index].status = 'allumé';
+  }
+
+  SwitchOffOne(index: number){
+    this.appareils[index].status = 'éteint';
+  }
 }
